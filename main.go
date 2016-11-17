70c4326eedeb3ab08d5ae417b7801935b1555e10
Andrew Darqui <andrew.darqui@gmail.com>
2016-11-17 14:37:12 -0500

    hacking stuff up part deux

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"fmt"
+	goredis "gopkg.in/redis.v4"
 	"log"
 	"os"
+	"os/exec"
 	"time"
 )
 
 func main() {
+	var out []byte
 
 	if len(os.Args) < 4 {
 		usage()
@@ -27,10 +31,40 @@ func main() {
 		log.Fatal("subscribe", err)
 	}
 	for {
-		msg, err := pubsub.ReceiveTimeout(time.Second * 1000000000)
+		msgi, err := pubsub.ReceiveTimeout(time.Second * 1000000000)
 		if err != nil {
 			log.Fatal("receiveMessage", err)
 		}
-		log.Printf("%v", msg)
+
+		msg, err := unwrapMessage(msgi)
+		if err != nil {
+			log.Println(err)
+		} else {
+			if msg != nil {
+				cmd := command
+				args := []string{msg.Channel, msg.Payload}
+				if out, err = exec.Command(cmd, args...).Output(); err != nil {
+					fmt.Fprintln(os.Stderr, err)
+				}
+				log.Printf("%s %s", msg.Channel, msg.Payload)
+				log.Printf("%s", out)
+			}
+		}
 	}
+}
+
+func unwrapMessage(msgi interface{}) (*goredis.Message, error) {
+
+	switch msg := msgi.(type) {
+	case *goredis.Subscription:
+		// ignore
+	case *goredis.Pong:
+		// ignore
+	case *goredis.Message:
+		return msg, nil
+	default:
+		return nil, fmt.Errorf("no message")
+
+	}
+	return nil, nil
 }
